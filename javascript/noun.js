80e7db9b98d2a6d71edf5bef35c557f0a4676aa3
Ben Dockery <benjadock@gmail.com>
2023-10-16 10:30:58 -0400

    Adds noun generator

---
+++ javascript/noun.js
@@ -0,0 +1,30 @@
+$(function () {
+  getWords();
+});
+
+function getWords() {
+  const nounsFile = fetch('/assets/text/nouns.txt')
+    .then(file => file.text())
+    .then(text => {return text.split('\n')});
+  const adjectivesFile = fetch('/assets/text/adjectives.txt')
+    .then(file => file.text())
+    .then(text => {return text.split('\n')});
+  Promise.all([nounsFile, adjectivesFile])
+    .then((data) => { processWords(data) });
+}
+
+function processWords(data) {
+  const nouns = data[0];
+  const adjectives = data[1];
+  $('.noun').each(function() {
+    console.log(this);
+    $(this).html(nouns[randomIndex(nouns.length)]);
+  });
+  $('.adjective').each(function() {
+    $(this).html(adjectives[randomIndex(adjectives.length)]);
+  });
+}
+
+function randomIndex(number) {
+  return Math.floor(Math.random() * number);
+}
